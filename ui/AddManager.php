e4fda8d920caa2b459a9f1f03c5e9884fbb8bff3
Jim Mason <jmason@ibinx.com>
2019-12-02 15:27:30 +0000

    JavaScript minification

--- ui/AddManager.php
+++ ui/AddManager.php
@@ -1242,12 +1242,14 @@ class AddManager extends MenuItem {
         UI::setFocus();
     ?>
     <SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript"><!--
+    <?php ob_start([\JSMin::class, 'minify']); ?>
     $().ready(function(){
         var INITIAL_SORT_COL = 0; //date
         $('.sortable-table').tablesorter({
             sortList: [[INITIAL_SORT_COL, 0]],
         });
     });
+    <?php ob_end_flush(); ?>
     // -->
     </SCRIPT>
 
