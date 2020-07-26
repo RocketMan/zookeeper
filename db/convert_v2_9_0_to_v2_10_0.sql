f501df778a7bb0f77a632a587922ee9344561fa0
ericg <eric.gilbertson@totvs.com>
2020-07-26 03:17:42 -0700

    added library track player
    added ability to play an album track from an associated URL
    
    closes #166

---
+++ db/convert_v2_9_0_to_v2_10_0.sql
@@ -0,0 +1,47 @@
+/*
+ * Zookeeper Online
+ *
+ * @author Jim Mason <jmason@ibinx.com>
+ * @copyright Copyright (C) 1997-2020 Jim Mason <jmason@ibinx.com>
+ * @link https://zookeeper.ibinx.com/
+ * @license GPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * version 3, along with this program.  If not, see
+ * http://www.gnu.org/licenses/
+ */
+
+/**
+ * IMPORTANT NOTE:
+ *
+ * Run this script ONLY if you are converting an existing Zookeeper Online
+ * v2_9_0 database for use with the current codebase.
+ *
+ * If you are creating a new database, run zkdbSchema.sql and then populate
+ * the resulting db using the various bootstrap scripts as appropriate.
+ */
+
+--
+-- Database: `zkdb`
+--
+
+SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;
+SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;
+SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION;
+SET NAMES utf8mb4;
+
+ALTER TABLE `tracknames` ADD COLUMN `url` varchar(2083) NOT NULL DEFAULT '';
+ALTER TABLE `colltracknames` ADD COLUMN `url` varchar(2083) NOT NULL DEFAULT '';
+
+SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT;
+SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS;
+SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION;
