de49b5e09eb1b657339755fa4ed6332a4234f5f0
Jim Mason <jmason@ibinx.com>
2020-08-03 15:15:53 +0100

    reverted to HTML 4.01 Transitional DTD; fixed mailto: link in home page

--- ui/Main.php
+++ ui/Main.php
@@ -87,7 +87,7 @@ class Main implements IController {
         $station = Engine::param('station');
         $station_full = Engine::param('station_full');
     ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <HTML>
 <HEAD>
   <TITLE><?php echo $banner;?></TITLE>
