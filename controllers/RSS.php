f8bae6f88d778f019edcc244ff5146609fe0365a
Jim Mason <jmason@ibinx.com>
2021-04-24 12:06:55 +0100

    updated copyright date

--- controllers/RSS.php
+++ controllers/RSS.php
@@ -3,7 +3,7 @@
  * Zookeeper Online
  *
  * @author Jim Mason <jmason@ibinx.com>
- * @copyright Copyright (C) 1997-2020 Jim Mason <jmason@ibinx.com>
+ * @copyright Copyright (C) 1997-2021 Jim Mason <jmason@ibinx.com>
  * @link https://zookeeper.ibinx.com/
  * @license GPL-3.0
  *
