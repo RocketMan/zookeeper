10b337475415e3b168ea439ff3b9565d26f2e685
Jim Mason <jmason@ibinx.com>
2021-12-26 19:01:56 +0000

    fixed label link metadata

--- jsonapi/Albums.php
+++ jsonapi/Albums.php
@@ -194,7 +194,7 @@ class Albums implements RequestHandlerInterface {
                 $relation = new Relationship("label", $res);
                 $relation->links()->set(new Link("related", Engine::getBaseUrl()."album/{$record["tag"]}/label"));
                 $relation->links()->set(new Link("self", Engine::getBaseUrl()."album/{$record["tag"]}/relationships/label"));
-                $relation->metaInformation()->set("name", $labels[0]["name"]);
+                $relation->metaInformation()->set("name", $res->attributes()->getOptional("name"));
                 $resource->relationships()->set($relation);
             }
         }
