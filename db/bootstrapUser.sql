baf76819cb3f5d3daa84c78005c2e88bd899081c
Jim Mason <jmason@ibinx.com>
2022-06-18 17:45:17 +0100

    updated boostrapUser for legacypass deprecation

--- db/bootstrapUser.sql
+++ db/bootstrapUser.sql
@@ -47,5 +47,5 @@ SET time_zone = "+00:00";
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `password`, `groups`, `realname`, `expires`, `legacypass`, `lastlogin`, `ssoaccount`) VALUES
-(1, 'root', 'a68bbd37621a42a19259a18f227dc9dbc3', 'mxncp', 'Zookeeper Superuser', NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`id`, `name`, `password`, `groups`, `realname`, `expires`, `lastlogin`, `ssoaccount`) VALUES
+(1, 'root', 'a68bbd37621a42a19259a18f227dc9dbc3', 'mxncp', 'Zookeeper Superuser', NULL, NULL, NULL);
