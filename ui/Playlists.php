0908546eca78ace0aa18ecce09b18c9c90c9164a
Jim Mason <jmason@ibinx.com>
2022-09-01 18:54:06 +0100

    revised playlist artist hint text for full text search
    fixes #330

--- ui/Playlists.php
+++ ui/Playlists.php
@@ -848,7 +848,7 @@ class Playlists extends MenuItem {
                     <div>
                         <label>Artist / Tag:</label>
                         <input required id='track-artist' autocomplete='off' maxlength=<?php echo PlaylistEntry::MAX_FIELD_LENGTH;?> data-focus />
-                        <span class='track-info' id='tag-status'>Artist (last name, first name) or tag number</span>
+                        <span class='track-info' id='tag-status'>Artist name or tag number</span>
                         <datalist id='track-artists'>
                         </datalist>
                     </div>
