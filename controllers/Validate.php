f38a362e2d0554b1a295d7936f7a8719c6129fb3
Jim Mason <jmason@ibinx.com>
2023-01-09 19:05:24 +0000

    Playlist add event:  Validate required fields are non-empty (#380)

--- controllers/Validate.php
+++ controllers/Validate.php
@@ -3,7 +3,7 @@
  * Zookeeper Online
  *
  * @author Jim Mason <jmason@ibinx.com>
- * @copyright Copyright (C) 1997-2022 Jim Mason <jmason@ibinx.com>
+ * @copyright Copyright (C) 1997-2023 Jim Mason <jmason@ibinx.com>
  * @link https://zookeeper.ibinx.com/
  * @license GPL-3.0
  *
@@ -300,6 +300,7 @@ class Validate implements IController {
             $this->showSuccess($success5, $response);
         }
 
+        $successd = false;
         if($this->doTest("duplicate playlist", $success4)) {
             $response = $this->client->post('api/v1/playlist', [
                 RequestOptions::JSON => [
