5ea476e97a1a47d982810a5730a1724bb99ef8bf
Jim Mason <jmason@ibinx.com>
2023-08-18 18:38:57 +0100

    version 3.0.0-rc.4

--- engine/Engine.php
+++ engine/Engine.php
@@ -3,7 +3,7 @@
  * Zookeeper Online
  *
  * @author Jim Mason <jmason@ibinx.com>
- * @copyright Copyright (C) 1997-2022 Jim Mason <jmason@ibinx.com>
+ * @copyright Copyright (C) 1997-2023 Jim Mason <jmason@ibinx.com>
  * @link https://zookeeper.ibinx.com/
  * @license GPL-3.0
  *
@@ -25,7 +25,7 @@
 namespace ZK\Engine;
 
 class Engine {
-    const VERSION = "3.0.0-rc.3";
+    const VERSION = "3.0.0-rc.4";
 
     const UA = "Zookeeper/2.0; (+https://zookeeper.ibinx.com/)";
 
