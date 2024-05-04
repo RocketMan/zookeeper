57ed15508ce97096c2a7b6314ad6f57a38e47db2
Jim Mason <jmason@ibinx.com>
2024-05-04 18:17:53 +0100

    finessed tag for mixed library/non-library reference

--- engine/impl/Playlist.php
+++ engine/impl/Playlist.php
@@ -846,7 +846,7 @@ class PlaylistImpl extends DBO implements IPlaylist {
         }
 
         $over = $airname?"distinct t.list":"*";
-        $query = "SELECT t.tag, count($over) plays, l.showdate, IFNULL(a.artist, t.artist) artist, t.album, t.label, count(*)" .
+        $query = "SELECT max(t.tag), count($over) plays, l.showdate, IFNULL(a.artist, t.artist) artist, t.album, t.label, count(*)" .
                  " FROM tracks t JOIN lists l ON t.list = l.id " .
                  ($zootopia ? " LEFT JOIN airnames n ON l.airname = n.id " : "") .
                  " LEFT JOIN albumvol a ON a.tag = t.tag " .
