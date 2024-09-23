2ed78a7da4805b0d472af684f112a2932d3f2cbc
Jim Mason <jmason@ibinx.com>
2024-09-23 19:12:17 +0100

    migrate playist editor and view playlist to twig

--- ui/PlaylistBuilder.php
+++ ui/PlaylistBuilder.php
@@ -32,13 +32,7 @@ class PlaylistBuilder extends PlaylistObserver {
     private const PARAMS = [ "action", "editMode", "authUser" ];
 
     protected $params;
-    protected $break;
-
-    protected static function timestampToLocale($timestamp) {
-        // colon is included in 24hr format for symmetry with fxtime
-        $timeSpec = UI::isUsLocale() ? 'h:i a' : 'H:i';
-        return $timestamp ? date($timeSpec, $timestamp) : '';
-    }
+    protected $template;
 
     public static function newInstance(array $params) {
         // validate all parameters are present
@@ -54,88 +48,35 @@ class PlaylistBuilder extends PlaylistObserver {
         return new PlaylistBuilder($params);
     }
 
-    protected function makeAlbumLink($entry, $includeLabel) {
-        $albumName = $entry->getAlbum();
-        $labelName = $entry->getLabel();
-
-        if(empty($albumName) && empty($labelName))
-            return "";
-
-        $albumTitle = $entry->getTag() ?
-            "<a href='?s=byAlbumKey&amp;n=" . htmlentities($entry->getTag()) .
-            "&amp;action=search' class='nav'>" . htmlentities($albumName) . "</a>" :
-            UI::smartURL($albumName);
-
-        if($includeLabel)
-            $albumTitle .= "<span class='songLabel'> / " . UI::smartURL($labelName) . "</span>";
-
-        return $albumTitle;
-    }
-
-    protected function makeEditDiv($entry) {
-        $href = "?id=" . $entry->getId() . "&amp;subaction=" . $this->params["action"] . "&amp;seq=editTrack";
-        $editLink = "<a class='songEdit nav' href='$href'><span class='fas fa-edit'></span></a>";
-        $dnd = "<div class='grab fas fa-grip-vertical' data-id='" . $entry->getId() . "'></div>";
-        return "<div class='songManager'>" . $dnd . $editLink . "</div>";
+    protected function renderBlock($block, $entry) {
+        return $this->template->renderBlock($block, [
+            "params" => $this->params,
+            "entry" => $entry
+        ]);
     }
 
     protected function __construct(array $params) {
+        $templateFact = new TemplateFactoryUI();
+        $this->template = $templateFact->load('list/block-event.html');
         $this->params = $params;
+        $this->params['break'] = false;
+        $this->params['usLocale'] = UI::isUsLocale();
         $this->on('comment', function($entry) {
-            $editCell = $this->params["editMode"] ? "<td>" .
-                $this->makeEditDiv($entry) . "</td>" : "";
-            $created = $entry->getCreatedTimestamp();
-            $timeplayed = self::timestampToLocale($created);
-            echo "<tr class='commentRow".($this->params["editMode"]?"Edit":"")."'>" . $editCell .
-                 "<td class='time' data-utc='$created'>$timeplayed</td>" .
-                 "<td colspan=4>".UI::markdown($entry->getComment()).
-                 "</td></tr>\n";
-            $this->break = false;
+            $fragment = $this->renderBlock('comment', $entry);
+            $this->params['break'] = false;
+            return $fragment;
         })->on('logEvent', function($entry) {
-            $created = $entry->getCreatedTimestamp();
-            $timeplayed = self::timestampToLocale($created);
-            if($this->params["authUser"]) {
-                // display log entries only for authenticated users
-                $editCell = $this->params["editMode"] ? "<td>" .
-                    $this->makeEditDiv($entry) . "</td>" : "";
-                echo "<tr class='logEntry".($this->params["editMode"]?"Edit":"")."'>" . $editCell .
-                     "<td class='time' data-utc='$created'>$timeplayed</td>" .
-                     "<td>".$entry->getLogEventType()."</td>" .
-                     "<td colspan=3>".$entry->getLogEventCode()."</td>" .
-                     "</tr>\n";
-                $this->break = false;
-            } else if(!$this->break) {
-                echo "<tr class='songDivider'>" .
-                     "<td class='time' data-utc='$created'>$timeplayed</td><td colspan=4><hr></td></tr>\n";
-                $this->break = true;
-            }
+            $fragment = $this->renderBlock('logEvent', $entry);
+            $this->params['break'] = !$this->params['authUser'];
+            return $fragment;
         })->on('setSeparator', function($entry) {
-            if($this->params["editMode"] || !$this->break) {
-                $editCell = $this->params["editMode"] ? "<td>" .
-                    $this->makeEditDiv($entry) . "</td>" : "";
-                $created = $entry->getCreatedTimestamp();
-                $timeplayed = self::timestampToLocale($created);
-                echo "<tr class='songDivider'>" . $editCell .
-                     "<td class='time' data-utc='$created'>$timeplayed</td><td colspan=4><hr></td></tr>\n";
-                $this->break = true;
-            }
+            $fragment = $this->renderBlock('setSeparator', $entry);
+            $this->params['break'] = true;
+            return $fragment;
         })->on('spin', function($entry) {
-            $editCell = $this->params["editMode"] ? "<td>" .
-                $this->makeEditDiv($entry) . "</td>" : "";
-            $created = $entry->getCreatedTimestamp();
-            $timeplayed = self::timestampToLocale($created);
-            $reviewCell = $entry->getReviewed() ? "<div class='albumReview'></div>" : "";
-            $artistName = $entry->getTag() ? PlaylistEntry::swapNames($entry->getArtist()) : $entry->getArtist();
-
-            $albumLink = $this->makeAlbumLink($entry, true);
-            echo "<tr class='songRow'>" . $editCell .
-                 "<td class='time' data-utc='$created'>$timeplayed</td>" .
-                 "<td>" . UI::smartURL($artistName) . "</td>" .
-                 "<td>" . UI::smartURL($entry->getTrack()) . "</td>" .
-                 "<td style='width: 15px'>$reviewCell</td>" .
-                 "<td>$albumLink</td>" .
-                 "</tr>\n";
-            $this->break = false;
+            $fragment = $this->renderBlock('spin', $entry);
+            $this->params['break'] = false;
+            return $fragment;
         });
     }
 }
