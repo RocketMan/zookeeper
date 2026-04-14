RocketMan/zookeeper

commit 6bea89dd69e02c58f6f59b2818edeb46fa3aef5d

finessed previous revision to allow top 30 with one chart

ui/Charts.php
@@ -78,25 +78,28 @@ class Charts extends MenuItem {
         $this->addVar('dateSpec', $dateSpec);
 
         $weeks = $chartAPI->getChartDates(2)->asArray();
-        if (count($weeks) != 2) {
+        if (count($weeks) < 1) {
             $this->setTemplate('charts/nocharts.html');
             return;
         }
 
         $thisWeek = $weeks[0]['week'];
-        $lastWeek = $weeks[1]['week'];
+        $lastWeek = count($weeks) > 1 ? $weeks[1]['week'] : null;
 
         // top 30
         $chart = [];
         $chartAPI->getChart($chart, '', $thisWeek, self::TOP_MAIN, '');
 
-        $last = [];
-        $chartAPI->getChart($last, '', $lastWeek, self::TOP_MAIN, '');
-        $this->mergeLast($chart, $last);
-
         $charts = [];
+
+        if ($lastWeek) {
+            $last = [];
+            $chartAPI->getChart($last, '', $lastWeek, self::TOP_MAIN, '');
+            $this->mergeLast($chart, $last);
+            $charts[$lastWeek][0] = $last;
+        }
+
         $charts[$thisWeek][0] = $chart;
-        $charts[$lastWeek][0] = $last;
 
         // genre charts
         $genres = [
@@ -117,12 +120,14 @@ class Charts extends MenuItem {
             $chart = [];
             $chartAPI->getChart($chart, '', $thisWeek, self::TOP_GENRE, $genre);
 
-            $last = [];
-            $chartAPI->getChart($last, '', $lastWeek, self::TOP_GENRE, $genre);
-            $this->mergeLast($chart, $last);
+            if ($lastWeek) {
+                $last = [];
+                $chartAPI->getChart($last, '', $lastWeek, self::TOP_GENRE, $genre);
+                $this->mergeLast($chart, $last);
+                $charts[$lastWeek][$genre] = $last;
+            }
 
             $charts[$thisWeek][$genre] = $chart;
-            $charts[$lastWeek][$genre] = $last;
         }
 
         $this->addVar('allcharts', $charts);

commit 5101fcb241e363ea2fd72f69cf732c6c2605da50

fixed regression in 6bea89d

ui/Charts.php
@@ -96,6 +96,7 @@ class Charts extends MenuItem {
             $last = [];
             $chartAPI->getChart($last, '', $lastWeek, self::TOP_MAIN, '');
             $this->mergeLast($chart, $last);
+            $charts[$thisWeek] = [];
             $charts[$lastWeek][0] = $last;
         }
 